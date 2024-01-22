ff5f4dcd76d9ba420cd5801a586d4ecf4a087e01
Chris Lombardi <clearchris@hotmail.com>
2024-01-22 16:31:44 -0500

    set end of day to 23:59:59 instead of 23:00:00

--- src/com/floreantpos/model/util/DateUtil.java
+++ src/com/floreantpos/model/util/DateUtil.java
@@ -39,8 +39,8 @@ public class DateUtil {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
 		cal.set(Calendar.HOUR_OF_DAY, 23);
-		cal.set(Calendar.MINUTE, 0);
-		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MINUTE, 59);
+		cal.set(Calendar.SECOND, 59);
 
 		return new Date(cal.getTimeInMillis());
 	}
